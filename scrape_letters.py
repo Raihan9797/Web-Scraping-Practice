8c1a8eda2d6e14eeca2f9193dcdb66e456280c30
Raihan9797 <raihanmuhd97@gmail.com>
2020-06-12 20:48:40 +0800

    converted code to function extract_letter_range()

--- scrape_letters.py
+++ scrape_letters.py
@@ -1,16 +1,10 @@
 """This uses the links from the .txt files to access each letter and scrape them to be saved."""
 
-# import json
-
-# test = 'practice/test.json'
-# with open(test) as file_object:
-    # descriptions = json.load(file_object)
-
+##### load json lists #####
 def json_loader(filename):
     import json
     with open(filename) as file_object:
         return json.load(file_object)
-
 
 descs_json = 'descriptions.json'
 links_json = 'links.json'
@@ -20,7 +14,9 @@ descriptions = json_loader(descs_json)
 names = json_loader(names_json)
 links = json_loader(links_json)
 
-# strips the links with '\n'
+
+
+##### strips the lists of '\n' ######
 def strip_lists(list_name):
     l = []
     for item in list_name:
@@ -32,36 +28,38 @@ names = strip_lists(names)
 descriptions = strip_lists(descriptions)
 
 
-########## letter extractor #########
-from bs4 import BeautifulSoup
-import requests
 
-for i in range(5, 11):
+##### letter extractor #####
+def extract_letter_range(start, end):
+    from bs4 import BeautifulSoup
+    import requests
 
-    # get source code from the link
-    link_source_code = requests.get(links[i]).text
+    for i in range(start ,end):
+        # get source code from the link
+        link_source_code = requests.get(links[i]).text
 
-    # get prettified soup
-    link_soup = BeautifulSoup(link_source_code, 'lxml')
-    print(link_soup.prettify)
+        # get prettified soup
+        link_soup = BeautifulSoup(link_source_code, 'lxml')
+        # print(link_soup.prettify)
 
-    # get title name and description
-    name = names[i]
-    desc = descriptions[i]
-    name_and_desc = f"{name}: {desc}"
+        # get title name and description
+        name = names[i]
+        desc = descriptions[i]
+        name_and_desc = f"{name}: {desc}"
 
-    # extract text from the soup
-    # add the name and description to the top of the text
-    link_soup_texts = link_soup.find_all('p')
-    link_text = f"{name}: {desc} \n"
+        # extract text from the soup
+        # add the name and description to the top of the text
+        link_soup_texts = link_soup.find_all('p')
+        link_text = f"{name}: {desc} \n"
+        for t in link_soup_texts:
+            # print(type(t.text))
+            link_text += t.text + '\n'
+        link_text
+        # print(link_text)
 
-    for t in link_soup_texts:
-        print(type(t.text))
-        link_text += t.text + '\n'
-    link_text
-    print(link_text)
+        # save it to a .txt file using its name and desc
+        file_name = f"all_letters/{name}_{desc}.txt"
+        with open(file_name, 'w') as fo:
+            fo.write(link_text)
 
-    # save it to a .txt file using its name and desc
-    file_name = f"all_letters/{name}_{desc}.txt"
-    with open(file_name, 'w') as fo:
-        fo.write(link_text)
+extract_letter_range(11, 20) # 11 to 20
