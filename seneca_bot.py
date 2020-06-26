95c3ecca442c4c490aff3d62be5f5ec2a0004863
Raihan9797 <raihanmuhd97@gmail.com>
2020-06-27 01:12:47 +0800

    bot able to get letters from a dict of 30 letters

--- seneca_bot.py
+++ seneca_bot.py
@@ -22,7 +22,8 @@ fn = 'meta_letters/Volume 2.txt'
 with open(fn, 'r') as fo:
     vol2 = fo.read()
 
-fn = 'all_letters/dict_0to10.json'
+# fn = 'all_letters/dict_0to10.json'
+fn = 'practice/thirty_letters.json'
 with open(fn, 'r') as fo:
     import json
     letters = json.load(fo)
